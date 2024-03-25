f52fdd3e16eccc304adfc126cc3fa4eca37526f0
Davy-Silva <157069929+Davy-Silva@users.noreply.github.com>
2024-03-25 19:13:56 -0300

    Concertando Erros JavaScript

--- exercicios/aula17-ex1/script.js
+++ exercicios/aula17-ex1/script.js
@@ -1,7 +1,7 @@
-var valores = []
-var num = document.querySelector('input#num')
-var lista = document.querySelector('table#lista')
-var res = document.querySelector('div#valores')
+let num = document.querySelector('input#fnum')
+let lista = document. querySelector('table#flista')
+let res = document.querySelector('div#res')
+let valores = []
 
 function isNumero(n)
 {
@@ -22,25 +22,7 @@ function inLista(n, l)
 function adicionar()
 {
     if(isNumero(num.value) && !inLista(num.value, valores))
-    {
-        valores.push(Number(num.value))
-        var linha = document.createElement('tr')
-        let item = document.createElement('td')
-        item.textContent = `Valor ${valores} adicionado`
-        tabuada.appendChild(linha)
-        linha.appendChild(item)
-    }
+    {   alert('Tudo ok') }
     else
-    {   alert('Valor inválido ou já encontrado na lista.') }
-}
-
-
-var arrayFim = array.length - 1
-
-function valores()
-{
-    res.innerHTML(`Ao todo, temos ${array.length} números cadastrados`)
-    res.innerHTML(`O maior valor informado foi ${array[arrayFim]}`)
-    res.innerHTML(`O menor valor infomado foi ${array[0]}`)
-    res.innerHTML(`Somando todos os valores temos`)
+    {   alert('Valor inválido ou já encontrado na lista') }
 }
