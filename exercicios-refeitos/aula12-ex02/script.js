87d27d0244994b818a5c9a82a3f06cd446c22525
Davy Silva Teles <157069929+Davy-Silva@users.noreply.github.com>
2024-04-04 18:34:45 -0300

    Testes JavaScript

--- exercicios-refeitos/aula12-ex02/script.js
+++ exercicios-refeitos/aula12-ex02/script.js
@@ -1,18 +1,36 @@
-function verficarIdade()
+function verificarIdade()
 {
     let ano = document.querySelector('input#id-ano')
     let anoNumber = Number(ano)
+    let anoAtual = new Date().getFullYear()
+    let idade = anoNumber - anoAtual
+    let texto = document.querySelector('p#id-texto')
+    let img = document.querySelector('img#id-img')
     let sexo = document.getElementsByName('sexo')
 
-    if(anoNumber >= 1920 && anoNumber <= 2024)
+    if(ano.value >= 1920 && ano.value <= 2024)
     {
-        if(sexo == M)
+        texto.innerHTML =`Encontramos um idoso com ${idade} anos`
+
+        if(sexo[0])
         {
-            switch
+            if(anoNumber <= 1960)
+            {
+               document.body.querySelector('div').img.background = 'imagens/adulto-f.jpg'
+            }
+            else if(anoNumber <= 2006)
+            {}
+            else
+            {}
         }
         else
         {
-
+            if(anoNumber <= 1960)
+            {}
+            else if(anoNumber <= 2006)
+            {}
+            else
+            {}
         }
     }
     else
