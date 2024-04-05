3b2af790efebc304db7df3a17eb11765010adebe
Davy Silva Teles <157069929+Davy-Silva@users.noreply.github.com>
2024-04-05 11:46:43 -0300

    Quebrando a Cabeça

--- exercicios-refeitos/aula12-ex02/script.js
+++ exercicios-refeitos/aula12-ex02/script.js
@@ -1,37 +1,49 @@
 function verificarIdade()
 {
     let ano = document.querySelector('input#id-ano')
-    let anoNumber = Number(ano)
+    let anoNumber = Number(ano.value)
     let anoAtual = new Date().getFullYear()
-    let idade = anoNumber - anoAtual
+    let idade = anoAtual - anoNumber
+    let res = document.querySelector('div#res')
     let texto = document.querySelector('p#id-texto')
-    let img = document.querySelector('img#id-img')
+    let img = new Image()
     let sexo = document.getElementsByName('sexo')
 
-    if(ano.value >= 1920 && ano.value <= 2024)
+    if(anoNumber>= 1920 && anoNumber <= 2024)
     {
-        texto.innerHTML =`Encontramos um idoso com ${idade} anos`
-
-        if(sexo[0])
+        if(sexo[0].checked)
         {
+            sexo = 'Homem'
+
             if(anoNumber <= 1960)
             {
-               document.body.querySelector('div').img.background = 'imagens/adulto-f.jpg'
+               img.src = 'imagens/idoso-m.jpg'
             }
             else if(anoNumber <= 2006)
             {}
             else
             {}
+
+            res.appendChild(img)
+
+            texto.innerHTML = `Encontramos um ${sexo} com ${idade} anos.`
         }
-        else
+        else if(sexo[1].checked)
         {
+            sexo = 'Mulher'
+
             if(anoNumber <= 1960)
             {}
             else if(anoNumber <= 2006)
             {}
             else
             {}
+
+            texto.innerHTML = `Encontramos um ${sexo} com ${idade} anos.`
         }
+        else
+        {   alert('Escolha seu sexo!') }
+
     }
     else
     {   alert('Idade Inválida!')}
