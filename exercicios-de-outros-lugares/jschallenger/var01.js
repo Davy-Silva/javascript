249f43fda146ce0a7d3db7c182da3403724b053a
Davy Silva Teles <157069929+Davy-Silva@users.noreply.github.com>
2024-04-30 19:44:41 -0300

    Exercicios JS

--- exercicios-de-outros-lugares/jschallenger/var01.js
+++ exercicios-de-outros-lugares/jschallenger/var01.js
@@ -35,3 +35,38 @@
 
     // TODOS OS VALORES QUE NAO ESTAO NESSA LISTA SAO VERDADEIROS
 }
+
+// OPERADORES
+{
+    // EQUAL
+
+    2 == 2 // TRUE
+    2 == 4 // FALSE
+
+    // NOT EQUAL
+
+    2 != 2 // FALSE
+    2 != 4 // TRUE
+
+    // GREATER THAN
+
+    2 > 1 // TRUE
+    2 > 4 // FALSE
+
+    // LESS THAN
+
+    2 < 1 // FALSE
+    2 < 4 // TRUE
+
+    // GREATER THAN OR EQUAL
+
+    2 >= 2 // TRUE
+    2 >= 4 // FALSE
+
+    // LESS THAN OR EQUAL
+
+    1 <= 2 // FALSE
+    2 <= 4 // TRUE
+
+    
+}
