102fc69fc8b8f621de7eca1808ad2b4c7dbbe0c4
Davy Silva Teles <157069929+Davy-Silva@users.noreply.github.com>
2024-04-30 11:40:17 -0300

    Devagar e Sempre

--- exercicios-de-outros-lugares/jschallenger/var01.js
+++ exercicios-de-outros-lugares/jschallenger/var01.js
@@ -21,4 +21,17 @@
 
     isOnSale = false
     console.log(inOnSale)
+
+    // CADA VALOR EM JAVASCRIPT E CONSIDERADO VERDADEIRO OU FALSO
+    // VALORES  FALSOS
+
+    falso
+    zero
+    indefinido
+    0
+    -0
+    Nan
+    Uma cadeia de caracteres vazia ('')
+
+    // TODOS OS VALORES QUE NAO ESTAO NESSA LISTA SAO VERDADEIROS
 }
